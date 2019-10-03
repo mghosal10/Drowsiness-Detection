6372381b89f57dba4f3a121fb504e601a5659c66
Gregory Wint <gregwint95@gmail.com>
2019-10-03 08:51:21 -0400

    Add code to detect facial features and compute eye aspect ratio

--- detection-api/detector.py
+++ detection-api/detector.py
@@ -1,3 +1,11 @@
+import dlib
+import cv2
+import numpy
+from imutils import face_utils
+from scipy.spatial import distance as dist
+import dotenv
+import os
+
 """
 A class to represent a machine capable of determing whether or not a
 person appears drowsy based on an ordered collection of images provided in
@@ -16,7 +24,9 @@ detect(images)
 """
 class DrowsinessDetector:
     def __init__(self):
+        dotenv.load_dotenv()
         self._consecutiveDrowsyFrames = 0
+        self._maxDrowsyFramesBeforeSignal = os.getenv("FRAMES_BEFORE_DROWSINESS_CONFIRMED")
 
     """
     Analyzes eyes appearing in images and makes a determination based on
@@ -30,4 +40,70 @@ class DrowsinessDetector:
         in the images can be considered drowsy, else False.
     """
     def detect(self, images):
+        ## Get facial landmarks (namely, left and right eyes)
+        landmarkDetector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor("/home/gregory/Downloads/shape_predictor_68_face_landmarks.dat")
+        img = dlib.load_rgb_image("testImage.png")
+        dets = landmarkDetector(img, 1)
+        print("num faces: ", len(dets))
+
+        # Find the 5 face landmarks we need to do the alignment.
+        faces = dlib.full_object_detections()
+        faces.append(predictor(img, dets[0]))
+
+        # grab the indexes of the facial landmarks for the left and
+        # right eye, respectively
+        (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+        (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+        print(faces)
+        shape = face_utils.shape_to_np(faces[0])
+        print(shape)
+
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = self.eye_aspect_ratio(leftEye)
+        rightEAR = self.eye_aspect_ratio(rightEye)
+
+        print(leftEye)
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(img, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(img, [rightEyeHull], -1, (0, 255, 0), 1)
+
+##        cv2.imshow("sklfjd", img)
+##        cv2.waitKey()
+
+        ear = (leftEAR + rightEAR) / 2
+        print(ear)
+
         return True
+
+
+    """
+    Calculates the eye-aspect ratio described in
+    http://vision.fe.uni-lj.si/cvww2016/proceedings/papers/05.pdf.  Eye-
+    aspect ratio is the ratio of height to width of the eyes.
+
+    @param eye ndarry An array describing the locations of landmarks within
+        an eye detected in an image.
+    @return double The ration between the height and width of an eye.
+    """
+    def eye_aspect_ratio(self, eye):
+        # compute the euclidean distances between the two sets of
+        # vertical eye landmarks (x, y)-coordinates
+        A = dist.euclidean(eye[1], eye[5])
+        B = dist.euclidean(eye[2], eye[4])
+
+        # compute the euclidean distance between the horizontal
+        # eye landmark (x, y)-coordinates
+        C = dist.euclidean(eye[0], eye[3])
+
+        ear = (A + B) / (2.0 * C)
+
+        return ear
+
+if __name__ == "__main__":
+    d = DrowsinessDetector()
+    d.detect([])
